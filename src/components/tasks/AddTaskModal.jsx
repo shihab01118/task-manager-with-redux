212cf699607bfd30973e4ab827553070a13c2d1a
shihab01118 <shahed.shihab.0@gmail.com>
2024-09-21 22:45:14 +0600

    add new task modal added

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,90 @@
+import { useForm } from "react-hook-form";
+import Modal from "../ui/Modal";
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const handleCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+    handleCancel();
+  };
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Add New Task">
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            className="rounded-md"
+            {...register("title", { required: true })}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="description">Description</label>
+          <textarea
+            type="text"
+            id="description"
+            className="rounded-md"
+            {...register("description", { required: true })}
+          ></textarea>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="date">Deadline</label>
+          <input
+            type="date"
+            id="date"
+            className="rounded-md"
+            {...register("deadline", { required: true })}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="assign-to">Assign To</label>
+          <select
+            id="assign-to"
+            className="rounded-md"
+            {...register("assignedTo", { required: true })}
+          >
+            <option value="Shahed Al Amin Shihab">Shahed Al Amin Shihab</option>
+            <option value="Maisha Tabassum Oishi">Maisha Tabassum Oishi</option>
+            <option value="Atik Shahriar">Atik Shahriar</option>
+            <option value="Faysal Neowaz">Faysal Neowaz</option>
+          </select>
+        </div>
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="priority">Priority</label>
+          <select
+            id="priority"
+            className="rounded-md"
+            {...register("priority", { required: true })}
+          >
+            <option value="high">
+              High
+            </option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <div className="flex justify-end gap-3">
+          <button
+            type="button"
+            onClick={() => handleCancel()}
+            className="btn btn-danger"
+          >
+            Cancel
+          </button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddTaskModal;
